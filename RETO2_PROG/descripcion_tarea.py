af8d287b59105a10e630f0032573ca43b54c872a
Evy <evybubble.1@gmail.com>
2025-10-09 09:59:40 -0500

    Update descripcion_tarea.py

--- RETO2_PROG/descripcion_tarea.py
+++ RETO2_PROG/descripcion_tarea.py
@@ -1,23 +1,5 @@
-# Se integrará una lista con las constantes y un diccionario con datos importantes de la simulación
 
-# Rendimiento de aeronave en diferentes condiciones
-rendimiento_simulación = {
-    "información de cohete": {
-        "Altura": 43,  # metros
-        "Diámetro": 7,  # metros
-        "Fairing": 5,  # metros
-    },
-    "Altitud": {  # 200 km
-        "velocidad_despegue1": 5, # km/min
-        "velocidad_despegue2": 3, # km/min
-        "Consumo_comb1": 800, #km/min
-        "Consumo_comb2": 500, #km/min
-    }
-}
-
-
-
-simulacion = ["objetivo altitud", "consumo etapa 1", "consumo etapa 2", "Incremento A1", "Imcremento A2"]
+# Simulaciones de lanzamiento de cohete
 
 def sim_cohete(combustible_e1, combustible_e2):
     objetivo_alt = 200
@@ -31,22 +13,17 @@ def sim_cohete(combustible_e1, combustible_e2):
     etapa = 1
 
     while altitud < objetivo_alt:
-        print(simulacion[0])
         if etapa == 1:
             if combustible_e1 > 0:
                 combustible_e1 -= consumo_1
-                print(simulacion[1])
                 altitud += IA1
-                print(simulacion[3])
                 minuto += 1
             else:
                 etapa = 2
         elif etapa == 2:
             if combustible_e2 > 0:
                 combustible_e2 -= consumo_2
-                print(simulacion[2])
                 altitud += IA2
-                print(simulacion[4])
                 minuto += 1
             else:
                 print("\nEl cohete se quedó sin combustible en la etapa 2.")
@@ -66,3 +43,6 @@ while True:
     if continuar == "NO":
         print("Simulación finalizada.")
         break
+
+
+
